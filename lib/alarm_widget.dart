eedab098e30c2f11b55b13e89c12e10827d0a261
Unknown <jonas.de.ridder@student.howest.be>
2019-04-10 14:53:19 +0200

    Add time picker

--- lib/alarm_widget.dart
+++ lib/alarm_widget.dart
@@ -1,18 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Alarm extends StatefulWidget {
-  Alarm({Key key}) :super(key: key);
+  Alarm({Key key}) : super(key: key);
 
   @override
   _AlarmState createState() => _AlarmState();
 }
 
 class _AlarmState extends State<Alarm> {
+  String alarm = "";
+  bool enabled;
+
+  @override
+  void initState() {
+    loadAlarm();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Alarm"),
+      child: Column(
+        children: <Widget>[
+          Text("Alarm"),
+          Row(
+            children: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.alarm),
+                label: Text(alarm),
+                onPressed: selectTime,
+                shape: Border.all(color: Colors.black),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text("On:"),
+              Checkbox(
+                value: enabled,
+                onChanged: (bool newValue) async {
+                  final prefs = await SharedPreferences.getInstance();
+                  prefs.setBool("alarmEnabled", newValue);
+                  setState(() {
+                    enabled = newValue;
+                  });
+                },
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 
+  loadAlarm() async {
+    final prefs = await SharedPreferences.getInstance();
+    String alarmString = prefs.getString("alarm");
+    if (alarmString == null) {
+      alarmString = "none";
+    }
+    bool enabledBool = prefs.getBool("alarmEnabled") ?? false;
+    setState(() {
+      alarm = alarmString;
+      enabled = enabledBool;
+    });
+    return;
+  }
+
+  selectTime() async {
+    TimeOfDay time =
+        await showTimePicker(context: context, initialTime: TimeOfDay.now());
+    String timeString = time.format(context);
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("alarm", timeString);
+
+    setState(() {
+      alarm = timeString;
+    });
+  }
 }
