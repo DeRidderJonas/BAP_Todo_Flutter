0853b2b42d8b1a3827aca4c63c823fd1734ff840
Unknown <jonas.de.ridder@student.howest.be>
2019-04-11 14:42:40 +0200

    Add list of tasks

--- lib/tasks_widget.dart
+++ lib/tasks_widget.dart
@@ -10,6 +10,18 @@ class TasksList extends StatefulWidget {
 }
 
 class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [];
+
+  @override
+  void initState() {
+    loadData().then((result){
+      setState(() {
+       tasks = result;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -20,9 +32,27 @@ class _TasksListState extends State<TasksList> {
             child: Text("Add new task"),
             onPressed: saveTask,
             shape: Border.all(color: Colors.black),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, position) {
+              return Row(
+                children: <Widget>[
+                  Text("${tasks[position].title}   (${tasks[position].done ? 'DONE' : 'TODO'})"),
+                  RaisedButton(
+                    child: Text("Set Active"),
+                    onPressed: () async{
+                      final prefs = await SharedPreferences.getInstance();
+                      prefs.setInt("currentTaskId", tasks[position].id);
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              );
+            },
+            itemCount: tasks.length,
           )
         ],
-        mainAxisAlignment: MainAxisAlignment.center,
       ),
     );
   }
@@ -39,5 +69,10 @@ class _TasksListState extends State<TasksList> {
     final prefs = await SharedPreferences.getInstance();
     prefs.setInt("currentTaskId", id);
     print('inserted row: $id');
+  }
+
+  loadData() async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    return await helper.getAll();
   }
 }
